27fc469ebf2bac5b1c6737950b3b1bce8da0c1ce
Muskan <42966101+LeoYoona@users.noreply.github.com>
2020-12-23 17:28:09 +0100

    reviwes

--- styling/main.js
+++ styling/main.js
@@ -1,13 +1,3 @@
-
-// function ToggleExtrasFunction(y) {
-//   var x = document.getElementById(y);
-//   if (x.style.display === "none") {
-//     x.style.display = "block";
-//   } else {
-//     x.style.display = "none";
-//   }
-// }
-
 $(document).ready(function() {
   $('.minus').click(function () {
     var $input = $(this).parent().find('input');
